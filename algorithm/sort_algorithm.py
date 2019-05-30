7fae5f7912a47cfc5f51b52d9dfe2879ec0ba886
weiye.xia <weiye.xia@pendanaan.com>
2019-05-30 19:58:36 +0800

    update

--- algorithm/sort_algorithm.py
+++ algorithm/sort_algorithm.py
@@ -113,7 +113,7 @@ def select_sort(arr):
         min_index = i
         for j in range(i+1, n):
             if arr[min_index] > arr[j]:
-                min_index = i
+                min_index = j
         if i != min_index:
             arr[min_index], arr[i] = arr[i], arr[min_index]
 
